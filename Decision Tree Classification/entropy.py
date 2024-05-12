9ad0f038539f46b99c8ec3aa9013500fa8622da7
yasuomaidana <yasuomaidana@gmail.com>
2024-05-12 16:24:01 +0900

    Calculating gain

--- Decision Tree Classification/entropy.py
+++ Decision Tree Classification/entropy.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy.random import random
+import pandas as pd
+from pandas import Series
 
 
 def generate_distribution(x, y, mu_x, mu_y, sigma_x, sigma_y):
@@ -15,14 +17,27 @@ def generate_distribution(x, y, mu_x, mu_y, sigma_x, sigma_y):
     return X, Y, Z
 
 
-def calculate_entropy(Z):
-    # Calculate the probability of Z
-    p = np.sum(Z) / Z.size
-
+def calculate_entropy(series: Series):
+    # Calculate the probability of each unique value in the series
+    probabilities = series.value_counts(normalize=True)
     # Calculate the entropy
-    if p == 0 or p == 1:
-        entropy = 0
-    else:
-        entropy = -p * np.log2(p) - (1 - p) * np.log2(1 - p)
-
+    entropy = -np.sum(probabilities * np.log2(probabilities))
     return entropy
+
+
+def calculate_gain(data, target, column, binning_func=pd.qcut, num_bins=8):
+    # Calculate the initial entropy of the target
+    initial_entropy = calculate_entropy(data[target])
+
+    # Check if the column is categorical
+    if not isinstance(data[column].dtype, pd.CategoricalDtype):
+        # Bin the column
+        data[f"{column}_tiles"] = binning_func(data[column], num_bins)
+        column = f"{column}_tiles"
+
+    column_entropy = calculate_entropy(data[column])
+
+    # Calculate the gain
+    gain = initial_entropy - column_entropy
+
+    return gain
