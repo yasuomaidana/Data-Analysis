75aabe997928f07c82d47f847759be4de92347b6
yasuomaidana <yasuomaidana@gmail.com>
2024-07-18 23:52:50 +0900

    Seed projects

--- Playing/Rick and Morty/data_loader.py
+++ Playing/Rick and Morty/data_loader.py
@@ -1,3 +1,3 @@
 import kaggle
 kaggle.api.authenticate()  # Authenticates using your kaggle.json
-kaggle.api.dataset_download_files('neuromusic/avocado-prices', path='./Data', unzip=True)
+kaggle.api.dataset_download_files('andradaolteanu/rickmorty-scripts', path='./Data', unzip=True)
