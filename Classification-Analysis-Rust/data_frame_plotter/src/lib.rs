5b25a05e99daf30669082fd88b4594d57942901d
yasuomaidana <yasuomaidana@gmail.com>
2024-08-03 15:44:09 +0900

    Generating relational plots from hue column name or hue color map

--- Classification-Analysis-Rust/data_frame_plotter/src/lib.rs
+++ Classification-Analysis-Rust/data_frame_plotter/src/lib.rs
@@ -10,31 +10,32 @@ use plotly::Scatter;
 use polars::frame::DataFrame;
 use polars::prelude::DataType::Float64;
 mod color_caster;
-use color_caster::generate_palette;
+use crate::color_caster::generate_color_map;
 
 pub fn cast_column_to_numeric(data: &DataFrame, column_name: &str) -> Vec<f64> {
     let column = data.column(column_name).expect("column not found");
     column.cast(&Float64).unwrap().f64().unwrap().into_iter().map(|v| v.unwrap()).collect()
 }
 
-// TO DO: Add the column
-pub fn single_relational_plot(x_name: &str, y_name: &str, hue: &str, data: &DataFrame) -> Box<Scatter<f64, f64>> {
-    let z = data.column(hue).expect("hue column not found");
 
-    let categories = z.unique().unwrap();
-    let colors = generate_palette(categories.len());
-
-    let categories_colors = categories.str().unwrap()
-        .iter().enumerate().map(|(i, v)| (v.unwrap(), colors[i]))
-        .collect::<HashMap<&str, Rgb>>();
-
-    let z = z.str()
+pub fn single_relational_plot_with_colors(
+    x_name: &str, y_name: &str, hue: &str,
+    data: &DataFrame, hue_colors: &HashMap<String, Rgb>) -> Box<Scatter<f64, f64>> {
+    let z = data.column(hue).expect("hue column not found")
+        .str()
         .unwrap().into_iter()
-        .map(|v| categories_colors[v.unwrap()]).collect::<Vec<Rgb>>();
+        .map(|v| hue_colors[v.unwrap()]).collect::<Vec<Rgb>>();
 
     Scatter::new(cast_column_to_numeric(data, x_name), cast_column_to_numeric(data, y_name))
         .mode(plotly::common::Mode::Markers)
         .marker(Marker::new().color_array(z))
+}
+
+// TO DO: Add the column
+pub fn single_relational_plot(x_name: &str, y_name: &str, hue: &str, data: &DataFrame) -> Box<Scatter<f64, f64>> {
+
+    let categories_colors = generate_color_map(hue, data);
+    single_relational_plot_with_colors(x_name, y_name, hue, data, &categories_colors)
 }
 
 #[cfg(test)]
