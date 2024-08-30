ace63b4eb20e681b44ac9d166d960ff1cb055298
yasuomaidana <yasuomaidana@gmail.com>
2024-08-31 01:34:51 +0900

    Scrapping part 2

--- Classification-Analysis-Rust/data-scrapper/src/main.rs
+++ Classification-Analysis-Rust/data-scrapper/src/main.rs
@@ -1,45 +1,28 @@
-use rand::prelude::SliceRandom;
 use scraper::{Html, Selector};
-use rand::thread_rng;
+use data_scrapper::remove_using_regex;
+use serde::Deserialize;
 
+#[derive(Debug, Deserialize)]
+struct Book {
+    title: String,
+    price: String,
+}
 
 fn main() {
-    let response = reqwest::blocking::get("https://en.wikipedia.org/wiki/List_of_states_of_Mexico")
+    let content = reqwest::blocking::get("https://books.toscrape.com/catalogue/page-1.html")
         .expect("Failed to send request");
-    let html_content = response.text().unwrap();
-    let html_document = Html::parse_document(&html_content);
-    let table_selector = Selector::parse("table.wikitable tbody tr").unwrap();
-    let html_table_rows = html_document.select(&table_selector);
-    let state_selector = Selector::parse("td:first-child").unwrap();
-    let city_selector = Selector::parse("td:nth-child(5)").unwrap();
-    let capital_selector = Selector::parse("td:nth-child(4)").unwrap();
-    let td_selector = Selector::parse("td").unwrap();
-    let mut states = Vec::new();
-    let mut capitals = Vec::new();
-    let mut cities = Vec::new();
+    let document = Html::parse_document(&content.text().expect("Failed to parse response"));
 
-    for row in html_table_rows{
-        let elements = row.select(&td_selector).count();
-        if elements<9{
-            continue;
-        }
-        let state = row.select(&state_selector).next().unwrap().text().next().unwrap();
-        states.push(state);
-        let capital = row.select(&capital_selector).next().unwrap().text().next().unwrap();
-        capitals.push(capital);
-        let city = if elements == 9 {
-            capital
-        }else{
-            row.select(&city_selector).next().unwrap().text().next().unwrap()
+    let book_selector = Selector::parse(".product_pod").unwrap();
+    let books = document.select(&book_selector);
+    for book in books {
+        let title = book.select(&Selector::parse("h3 a").unwrap()).next().unwrap().inner_html();
+        let price = book.select(&Selector::parse(".price_color").unwrap()).next().unwrap().inner_html();
+        let book = Book {
+            title: remove_using_regex(&title, r"[^a-zA-Z0-9 ]", ""),
+            price: remove_using_regex(&price, r"[^0-9.]", ""),
         };
-        cities.push(city);
+        println!("{:?}", book);
     }
-    let mut all_elements = Vec::new();
-    all_elements.extend(cities);
-    all_elements.extend(states);
-    all_elements.extend(capitals);
 
-    let mut rng = thread_rng();
-    all_elements.shuffle(&mut rng);
-    println!("{}", all_elements.join(", "))
 }
