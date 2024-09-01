16aaef7c8afb4b509f790d845d41a03abf9e68c3
yasuomaidana <yasuomaidana@gmail.com>
2024-09-01 21:28:54 +0900

    Moving train and test inside the model

--- Classification-Analysis-Rust/tch-tensor-example/src/model.rs
+++ Classification-Analysis-Rust/tch-tensor-example/src/model.rs
@@ -4,68 +4,27 @@ use tch::nn::{conv2d, linear, Adam, Conv2D, Linear, Optimizer, OptimizerConfig, 
 use tch::{kind, nn::ModuleT, Device, Kind, TchError, Tensor};
 
 // Renaming variables to follow Rust conventions
-struct MnistCnn {
+pub(crate) struct MnistCnn {
     conv_layer1: Conv2D,
     conv_layer2: Conv2D,
     fully_connected1: Linear,
     fully_connected2: Linear,
 }
 
-pub fn generate_random_index(array_size: i64, batch_size: i64) -> Tensor{
+pub fn generate_random_index(array_size: i64, batch_size: i64) -> Tensor {
     let random_indices = Tensor::randint(array_size, &[batch_size], kind::INT64_CPU);
     random_indices
 }
 
-fn train(model: &mut MnistCnn, vs: &VarStore, train_data: &Tensor,
-         train_labels: &Tensor,
-         train_size: usize,
-         val_data: &Tensor,
-         val_labels: &Tensor,
-         batch_size: i64,
-         n_epochs: i64,
-         optimizer: &mut Optimizer,
-         criterion: &dyn Fn(&Tensor, &Tensor) -> Tensor) {
-
-    // set up optimizer
-    let n_it = (train_size as i64)/batch_size; // already round down
-    println!("Number of iteration with given batch size: {:?}", n_it);
-    // run epochs
-    for epoch in 0..n_epochs {
-        // generate random indices for batch size
-        // run all the images divided in batches  -> for loop
-        for _ in 0..n_it {
-            // get the batch
-            let batch_indices = generate_random_index(train_size as i64, batch_size);
-            let batch_images = train_data.index_select(0, &batch_indices).to_device(vs.device()).to_kind(Kind::Float);
-            let batch_labels = train_labels.index_select(0, &batch_indices).to_device(vs.device()).to_kind(Kind::Int64);
-            // forward pass
-            let output = model.forward_t(&batch_images, true);
-            // compute loss
-            let loss = criterion(&output, &batch_labels);
-            // compute gradients
-            optimizer.backward_step(&loss);
-        }
-        // compute accuracy
-        let val_accuracy =
-            model.batch_accuracy_for_logits(&val_data, &val_labels, vs.device(), 1024);
-        println!("epoch: {:4} test acc: {:5.2}%", epoch, 100. * val_accuracy);
-    }
-
-}
-
-fn test(model: &MnistCnn, vs: &VarStore, test_data: &Tensor, test_labels: &Tensor) -> f64 {
-    // test accuracy
-    let test_accuracy = model.batch_accuracy_for_logits(&test_data, &test_labels, vs.device(), 1024);
-    println!("Final test accuracy {:5.2}%", 100.*test_accuracy);
-    test_accuracy
-}
-
-fn with_no_grad<F>(f: F) where F: FnOnce() {
+fn with_no_grad<F>(f: F)
+where
+    F: FnOnce(),
+{
     tch::no_grad(f);
 }
 
 impl MnistCnn {
-    fn new(vs: &VarStore) -> Self {
+    pub(crate) fn new(vs: &VarStore) -> Self {
         let root = &vs.root();
         let conv_layer1 = conv2d(root, 1, 32, 5, Default::default());
         let conv_layer2 = conv2d(root, 32, 64, 5, Default::default()); // Assuming input size remains consistent
@@ -77,7 +36,7 @@ impl MnistCnn {
         MnistCnn { conv_layer1, conv_layer2, fully_connected1, fully_connected2 }
     }
     // Function to save the model
-    pub fn save(&self, filename: &str, vs:&VarStore) -> Result<(), TchError> {
+    pub fn save(&self, filename: &str, vs: &VarStore) -> Result<(), TchError> {
         let path = Path::new(filename);
         vs.save(path)?;
         Ok(())
@@ -88,6 +47,49 @@ impl MnistCnn {
         let path = Path::new(filename);
         vs.load(path)?;
         Ok(MnistCnn::new(vs))
+    }
+
+    pub fn test(&self, test_data: &Tensor, test_labels: &Tensor, device: Device) -> f64 {
+        // test accuracy
+        self.batch_accuracy_for_logits(test_data, test_labels, device, 1024)
+    }
+
+    pub fn train(&mut self, train_data: &Tensor,
+                 train_labels: &Tensor,
+                 train_size: usize,
+                 val_data: &Tensor,
+                 val_labels: &Tensor,
+                 batch_size: i64,
+                 n_epochs: i64,
+                 optimizer: &mut Optimizer,
+                 criterion: &dyn Fn(&Tensor, &Tensor) -> Tensor,
+                 device: Device,
+    ) {
+
+        // set up optimizer
+        let n_it = (train_size as i64) / batch_size; // already round down
+        println!("Number of iteration with given batch size: {:?}", n_it);
+        // run epochs
+        for epoch in 0..n_epochs {
+            // generate random indices for batch size
+            // run all the images divided in batches  -> for loop
+            for _ in 0..n_it {
+                // get the batch
+                let batch_indices = generate_random_index(train_size as i64, batch_size);
+                let batch_images = train_data.index_select(0, &batch_indices).to_device(device).to_kind(Kind::Float);
+                let batch_labels = train_labels.index_select(0, &batch_indices).to_device(device).to_kind(Kind::Int64);
+                // forward pass
+                let output = &self.forward_t(&batch_images, true);
+                // compute loss
+                let loss = criterion(&output, &batch_labels);
+                // compute gradients
+                optimizer.backward_step(&loss);
+            }
+            // compute accuracy
+            let val_accuracy =
+                &self.batch_accuracy_for_logits(&val_data, &val_labels, device, 1024);
+            println!("epoch: {:4} test acc: {:5.2}%", epoch, 100. * val_accuracy);
+        }
     }
 }
 
@@ -207,27 +209,24 @@ mod tests {
     #[test]
     fn test_train_and_test_model() {
         let device = Device::Mps; // or Device::Cuda(0) if CUDA is available
+
         let vs = VarStore::new(device);
         let mut model = MnistCnn::new(&vs);
         let mut optimizer = Adam::default().build(&vs, 0.001).unwrap();
         let criterion = |x: &Tensor, y: &Tensor| x.cross_entropy_for_logits(y);
 
-        // Creating random datasets for simplicity
-        let train_data = Tensor::randn(&[64, 1, 28, 28], (Kind::Float, device)); // 64 random images
-        let train_labels = Tensor::randint(10, &[64], (Kind::Int64, device)); // 64 random labels
+        // Dummy data creation
+        let train_data = Tensor::randn(&[64, 1, 28, 28], (Kind::Float, device));
+        let train_labels = Tensor::randint(10, &[64], (Kind::Int64, device));
+        let val_data = Tensor::randn(&[64, 1, 28, 28], (Kind::Float, device));
+        let val_labels = Tensor::randint(10, &[64], (Kind::Int64, device));
 
-        let test_data = Tensor::randn(&[64, 1, 28, 28], (Kind::Float, device));
-        let test_labels = Tensor::randint(10, &[64], (Kind::Int64, device));
+        let criterion = |x: &Tensor, y: &Tensor| x.cross_entropy_for_logits(y);
 
-        let start_time = Instant::now();
-        train(&mut model, &vs, &train_data, &train_labels, 64, &test_data, &test_labels, 64, 5, &mut optimizer, &criterion);
-        let elapsed_time = start_time.elapsed();
+        model.train(&train_data, &train_labels, 64, &val_data, &val_labels, 32, 2, &mut optimizer, &criterion, device);
 
-        println!("Training time: {:?}", elapsed_time);
-
-        let accuracy = test(&model, &vs, &test_data, &test_labels);
-        println!("Test accuracy: {}", accuracy);
-        assert_ne!(accuracy, 0.0, "Model accuracy should be something different than 0.0");
+        let accuracy = model.test(&val_data, &val_labels, device);
+        println!("Validation accuracy: {}", accuracy);
+        assert_ne!(accuracy, 0.0, "Model should have some accuracy");
     }
-
 }
