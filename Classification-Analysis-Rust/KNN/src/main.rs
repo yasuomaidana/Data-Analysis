08fce68f53386c138227deab0ce83285df5335d4
yasuomaidana <yasuomaidana@gmail.com>
2024-10-25 22:30:34 -0600

    Loading data from kaggle

--- Classification-Analysis-Rust/KNN/src/main.rs
+++ Classification-Analysis-Rust/KNN/src/main.rs
@@ -7,8 +7,10 @@ use polars::prelude::DataFrame;
 use polars_io::prelude::{CsvReadOptions, CsvReader};
 use polars_io::SerReader;
 use std::path::PathBuf;
+use tokio_compat_02::FutureExt;
+use data_loader::{download_file, KaggleFile};
 
-fn plotting_relational_plot(x_name: &str, y_name: &str, z_name: &str, data: &DataFrame) {
+fn plotting_relational_plot(x_name: &str, y_name: &str, z_name: &str, data: &DataFrame, title: &str) {
     let traces = single_relational_plot(x_name, y_name, z_name, data);
 
     let x_axis = Vec::from([Some(Box::new(Axis::new().title(Title::from(x_name))))]);
@@ -22,29 +24,40 @@ fn plotting_relational_plot(x_name: &str, y_name: &str, z_name: &str, data: &Dat
     let layout = Layout::new()
         .x_axis(x_axis)
         .y_axis(y_axis)
-        .title(Title::from("My Plot title"));
+        .title(Title::from(title));
     plot.set_layout(layout);
 
     // plot.show(); showup the plot in the browser
     plot.write_image("KNN/relational_plot.png", PNG, 800, 600, 1.0);
 }
 
-fn main() {
-    let filename = "data/KNNAlgorithmDataset.csv";
-    let path = PathBuf::from(filename);
+#[tokio::main]
+async fn main() {
+    let kaggle_file = KaggleFile::new_csv(
+        "uciml/iris".to_string(),
+        "Iris".to_string());
+    download_file(kaggle_file.clone()).compat().await;
+    let filename = kaggle_file.full_name();
+    let path = PathBuf::from(&filename);
 
     let csv_options = CsvReadOptions::default()
         .with_has_header(true)
+        // .with_skip_rows(2)
         .try_into_reader_with_file_path(Some(path))
         .unwrap();
 
     let data_frame = CsvReader::try_from(csv_options)
-        .expect(format!("Error reading file: {}", filename).as_str())
+        .expect(format!("Error reading file: {}", &filename).as_str())
         .finish()
-        .expect(format!("Error reading file: {}", filename).as_str());
+        .expect(format!("Error reading file: {}", &filename).as_str());
 
     println!("{:?}", data_frame);
-
+    let data_frame = data_frame.drop_nulls::<String>(None).expect("Failed to drop null values");
+    println!("{:?}", data_frame.get_column_names());
     // Plotting the data
-    plotting_relational_plot("radius_mean", "texture_mean", "diagnosis", &data_frame);
+    plotting_relational_plot("SepalLengthCm",
+                             "SepalWidthCm",
+                             "Species", &data_frame,
+                             "Iris Dataset");
+
 }
