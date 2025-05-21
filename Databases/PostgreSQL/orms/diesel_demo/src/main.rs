bc381a773040618fcc20ae9e7857612ee1f88998
yasuomaidana <yasuomaidana@gmail.com>
2025-05-21 09:59:03 -0600

    COUR-1948: Starting the diesel project

--- Databases/PostgreSQL/orms/diesel_demo/src/main.rs
+++ Databases/PostgreSQL/orms/diesel_demo/src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct MainCommand {
+    #[clap(short, long, value_parser, default_value = ".env")]
+    config_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = MainCommand::parse();
+    let config_file = cmd.config_file;
+    let config_content = std::fs::read_to_string(config_file)
+        .expect("Failed to read the config file");
+    println!("Config file content:\n{}", config_content);
+    
 }
