c6e6f5769c0fed74bc0ee0801ab6dc12d2d7d127
yasuomaidana <yasuomaidana@gmail.com>
2025-05-21 10:35:35 -0600

    COUR-1948: Adding configuration file to project

--- Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/main.rs
+++ Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/main.rs
@@ -1,22 +1,24 @@
 mod commands_implementations;
 
+use std::path::PathBuf;
 use crate::commands_implementations::download;
 use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
 struct RawCSVCommands {
+    #[clap(short, long, value_parser, default_value = ".env")]
+    config:PathBuf,
     #[command(subcommand)]
     command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Download {
-        url: String,
-    },
+    Download { url: String },
     Delete,
     Upload,
+    CreateTable,
 }
 
 impl Commands {
@@ -25,6 +27,7 @@ impl Commands {
             Commands::Download { url } => download(url),
             Commands::Delete => println!("Deleting..."),
             Commands::Upload => println!("Uploading..."),
+            Commands::CreateTable => println!("Creating table..."),
         }
     }
 }
