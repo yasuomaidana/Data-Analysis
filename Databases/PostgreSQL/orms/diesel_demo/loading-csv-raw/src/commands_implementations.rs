b0c528e219f8c0b3ecdadc1709a50217f6e7c25b
yasuomaidana <yasuomaidana@gmail.com>
2025-05-22 10:29:12 -0600

    COUR-1948: Implementing copy command

--- Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/commands_implementations.rs
+++ Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/commands_implementations.rs
@@ -1,7 +1,8 @@
-use crate::postgres_config::read_conf_from_env;
-use postgres::{Client, NoTls};
+use crate::postgres_config::{PostgresConfig, read_conf_from_env};
+use postgres::{Client, Config, GenericClient, NoTls};
 use reqwest::blocking;
 use std::fs::File;
+use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
 pub fn download(url: &String) {
@@ -39,4 +40,27 @@ pub fn create_table(config: &PathBuf) {
         .expect("Failed to create table.");
     println!("Database config: {:?}", database_config);
     println!("Table created.");
+}
+
+pub fn copy_csv_to_postgres(config: PostgresConfig, csv_file_path: &str) {
+    let connection_string = config.get_connection_string();
+    println!("{:?}", config);
+    println!("{connection_string}");
+    let mut client =
+        Client::connect(&connection_string, NoTls).expect("Failed to connect to the database");
+    
+    // Start the COPY stream
+    let mut copy_in = client
+        .copy_in("COPY track_raw(title, artist, album, count, rating, len) FROM STDIN WITH CSV")
+        .expect("Failed to start COPY");
+
+    // Open and read the CSV file
+    let file = File::open(csv_file_path).expect("Failed to open CSV");
+    let mut reader = BufReader::new(file);
+
+    // Stream contents to the COPY process
+    std::io::copy(&mut reader, &mut copy_in).expect("Failed to copy data to database");
+    copy_in.finish().expect("Failed to finish copy");
+
+    println!("Data successfully imported from '{}'.", csv_file_path);
 }
--- Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/main.rs
+++ Databases/PostgreSQL/orms/diesel_demo/loading-csv-raw/src/main.rs
@@ -17,7 +17,10 @@ struct RawCSVCommands {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Download { url: String },
-    Delete,
+    Delete {
+        #[clap(short, long, default_value_t = false)]
+        drop: bool
+    },
     Upload,
     CreateTable,
 }
@@ -28,11 +31,14 @@ impl RawCSVCommands {
             Commands::Download { url } => {
                 download(url);
             }
-            Commands::Delete => {
+            Commands::Delete{drop} => {
                 println!("Deleting...");
             }
             Commands::Upload => {
                 println!("Uploading...");
+                let database_config = postgres_config::read_conf_from_env(&self.config);
+                let csv_file_path = "library.csv";
+                commands_implementations::copy_csv_to_postgres(database_config, csv_file_path);
             }
             Commands::CreateTable => {
                 create_table(&self.config);
