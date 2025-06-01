6933dd4472c880f224ff306435081d8bfe7ff00f
yasuomaidana <yasuomaidana@gmail.com>
2025-06-01 12:53:11 -0600

    COUR-1958: Creating show albums and tracks

---
+++ Databases/PostgreSQL/orms/python-postgresql/one_to_many/commands/show_albums.py
@@ -0,0 +1,19 @@
+import click
+from click import option
+from sqlmodel import Session, select, create_engine
+from ..model import Album
+
+@click.command("show-albums")
+@click.pass_context  # Decorate 'version_command' to receive context
+@option('--limit','-l', type=int)
+def show_albums(ctx: click.Context, limit: int):
+    """Shows all/limit albums."""
+    click.echo("Showing albums...")
+    engine = create_engine(ctx.obj['DATABASE_URL'], echo=True)
+    with Session(engine) as session:
+        statement = select(Album).order_by(Album.name)
+        if limit:
+            statement = statement.limit(limit)
+        albums = session.exec(statement).all()
+        for album in albums:
+            click.echo(f"id: {album.id} title: {album.title}")
