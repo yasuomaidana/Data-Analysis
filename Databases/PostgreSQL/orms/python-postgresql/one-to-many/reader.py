d28dfaefa156716764fc6213206f05a932e6ae39
yasuomaidana <yasuomaidana@gmail.com>
2025-05-31 19:02:32 -0600

    COUR-1958: Returning albums and tracks from database

--- Databases/PostgreSQL/orms/python-postgresql/one-to-many/reader.py
+++ Databases/PostgreSQL/orms/python-postgresql/one-to-many/reader.py
@@ -1,11 +1,18 @@
 import csv
+from model import Album, Track
+
 
 def read_csv_file(filename):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            yield row
-            
+            album = Album(name=row[2])
+            track = Track(title=row[0],
+                          len=row[3],
+                          rating=row[4],
+                          count=row[5])
+            yield track, album
+
+
 if __name__ == '__main__':
     print(list(read_csv_file('../library.csv')))
-        
