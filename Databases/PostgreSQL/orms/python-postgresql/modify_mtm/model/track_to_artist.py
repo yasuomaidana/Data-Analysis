cef0c92732bb758866e27a9a7cb6c75b0df1048c
yasuomaidana <yasuomaidana@gmail.com>
2025-06-09 17:50:09 -0600

    COUR-1958: Adding todo for sqlmodel

--- Databases/PostgreSQL/orms/python-postgresql/modify_mtm/model/track_to_artist.py
+++ Databases/PostgreSQL/orms/python-postgresql/modify_mtm/model/track_to_artist.py
@@ -1,9 +1,14 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 
+
 class TrackToArtist(SQLModel, table=True):
     __tablename__ = "tracktoartist"
 
     id: Optional[int] = Field(default=None, primary_key=True)
     track_id: Optional[int] = Field(default=None, foreign_key="track.id")
     artist_id: Optional[int] = Field(default=None, foreign_key="artist.id")
+
+    # TODO: Add relationships to Track and Artist models, e\.g\.
+    # track: Optional\["Track"\] = Relationship(back_populates="track_to_artists")
+    # artist: Optional\["Artist"\] = Relationship(back_populates="track_to_artists")
