335603410b0f4750d69972ec16ab705e0b23f106
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2025-12-18 17:11:09 -0600

    COUR-1961: feature: implement create method for NewCar struct with input sanitization

--- Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/car.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/car.rs
@@ -1,7 +1,7 @@
 use crate::schema::car_schema::cars;
-use diesel::{Insertable, Queryable, Selectable};
+use diesel::{Insertable, PgConnection, Queryable, RunQueryDsl, Selectable};
 
-#[derive(Queryable, Selectable)]
+#[derive(Queryable, Selectable, Debug)]
 #[diesel(table_name = cars)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Car {
@@ -10,9 +10,21 @@ pub struct Car {
     pub year: i32,
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Clone)]
 #[diesel(table_name = cars)]
 pub struct NewCar {
     pub model: String,
     pub year: i32,
+}
+
+impl NewCar {
+    pub fn create(&self, conn: &mut PgConnection) -> Car {
+        let mut sanitized = self.clone();
+        sanitized.model = sanitized.model.trim().to_string();
+
+        diesel::insert_into(cars::table)
+            .values(sanitized)
+            .get_result(conn)
+            .expect("Error saving new car")
+    }
 }
