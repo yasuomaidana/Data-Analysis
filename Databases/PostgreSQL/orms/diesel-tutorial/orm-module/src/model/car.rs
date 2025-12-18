00a3f3e1cecf0174291fc9ccb4fec86a26b4e43c
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2025-12-18 16:32:56 -0600

    COUR-1961: feature: add NewCar struct for inserting new car records

--- Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/car.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/car.rs
@@ -1,10 +1,18 @@
-use diesel::{Queryable, Selectable};
+use crate::schema::car_schema::cars;
+use diesel::{Insertable, Queryable, Selectable};
 
 #[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::car_schema::cars)]
+#[diesel(table_name = cars)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct Car {
     pub id: i32,
+    pub model: String,
+    pub year: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = cars)]
+pub struct NewCar {
     pub model: String,
     pub year: i32,
 }
