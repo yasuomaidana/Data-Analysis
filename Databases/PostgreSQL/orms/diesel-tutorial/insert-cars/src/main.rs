95ca4c79dd84e93cdbe8026d999d0c5165b6fa55
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2025-12-27 09:01:08 -0600

    COUR-1961: feature: enhance car insertion with batch processing and improved input handling

--- Databases/PostgreSQL/orms/diesel-tutorial/insert-cars/src/main.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/insert-cars/src/main.rs
@@ -1,21 +1,37 @@
-use orm_module::model::car::NewCar;
+use diesel::dsl::insert_into;
+use diesel::{RunQueryDsl, SelectableHelper};
+use orm_module::model::car::{Car, NewCar};
+use orm_module::schema::car_schema::cars::dsl::cars;
+use utils::reader::{get_arg, read_line};
 
 fn get_new_car() -> NewCar {
-    let mut model = String::new();
-    let mut year_std = String::new();
-
     println!("Which model do you want to load?");
-    std::io::stdin().read_line(&mut model).unwrap();
+    let model: String = read_line().unwrap();
     println!("Which year is the new model?");
-    std::io::stdin().read_line(&mut year_std).unwrap();
-
-    let year = year_std.trim().parse().expect("Couldn't parse the year");
+    let year: i32 = read_line().unwrap();
     NewCar { model, year }
 }
 
 fn main() {
-    let to_store = get_new_car();
     let mut conn = orm_module::establish_connection();
-    let stored = to_store.create(&mut conn);
-    println!("Stored car {:?}", stored);
+
+    let items = get_arg::<u32>(&std::env::args().collect(), 1);
+    match items {
+        Ok(Some(n)) => {
+            let new_cars = (0..n).map(|_| get_new_car()).collect::<Vec<_>>();
+            let stored_cars = insert_into(cars)
+                .values(&new_cars)
+                .returning(Car::as_returning())
+                .get_results(&mut conn);
+            println!("Stored cars: {:?}", stored_cars);
+        }
+        Ok(None) => {
+            let to_store = get_new_car();
+            let stored = to_store.create(&mut conn);
+            println!("Stored car {:?}", stored);
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
 }
