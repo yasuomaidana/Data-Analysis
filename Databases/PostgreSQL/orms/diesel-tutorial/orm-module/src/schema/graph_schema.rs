b008419e794b6099120fc76803bd887a7b2dd85c
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2026-01-10 09:11:01 -0600

    COUR-1961: feature: add graph schema definitions for entities and relationships

---
+++ Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/schema/graph_schema.rs
@@ -0,0 +1,20 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    entities (id) {
+        id -> Text,
+        _type -> Text,
+        _class -> Text,
+        metadata -> Nullable<Jsonb>,
+    }
+}
+
+diesel::table! {
+    relationships (source_entity_id, target_entity_id) {
+        source_entity_id -> Text,
+        target_entity_id -> Text,
+        _class -> Nullable<Text>,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(entities, relationships,);
