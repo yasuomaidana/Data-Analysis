0b49f77d60149d3b12a2ceaf715c4064a5fb3993
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2026-01-10 11:07:57 -0600

    COUR-1961: feature: add Relationship struct and update dependencies in graph schema

--- Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/graph.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/orm-module/src/model/graph.rs
@@ -1,5 +1,5 @@
-use crate::schema::graph_schema::entities;
-use diesel::{Identifiable, Insertable, Queryable, Selectable};
+use crate::schema::graph_schema::{entities, relationships};
+use diesel::{Associations, Identifiable, Insertable, Queryable, Selectable};
 
 #[derive(Identifiable, Selectable, Queryable, Debug, Insertable)]
 #[diesel(table_name = entities)]
@@ -8,4 +8,13 @@ pub struct Entity {
     pub _type: String,
     pub _class: String,
     pub metadata: Option<serde_json::Value>,
+}
+
+#[derive(Identifiable, Selectable, Queryable, Associations, Debug, Insertable)]
+#[diesel(belongs_to(Entity, foreign_key = source_entity_id))]
+#[diesel(primary_key(source_entity_id, target_entity_id))]
+pub struct Relationship {
+    pub source_entity_id: String,
+    pub target_entity_id: String,
+    pub _class: Option<String>,
 }
