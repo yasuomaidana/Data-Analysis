9b182908b354cc307254808d26b4c8fb4964ba22
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2026-01-09 20:04:30 -0600

    COUR-1961: feature: enhance get_enrollments command with detailed student course listings

--- Databases/PostgreSQL/orms/diesel-tutorial/relations/src/many_to_many/get_enrollments.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/relations/src/many_to_many/get_enrollments.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use diesel::associations::HasTable;
-use diesel::{BelongingToDsl, QueryDsl, RunQueryDsl, SelectableHelper};
-use orm_module::model::course::{Course, Enrollment};
+use diesel::{BelongingToDsl, ExpressionMethods, GroupedBy};
+use diesel::{QueryDsl, RunQueryDsl, SelectableHelper};
+use itertools::Itertools;
+use orm_module::model::course::{Course, Enrollment, Student, StudentData};
 use orm_module::model::user::User;
 use orm_module::schema::courses_schema::courses;
 use orm_module::schema::courses_schema::enrollments;
@@ -18,9 +19,17 @@ struct Args {
 
     #[arg(help = "Course title", short = 'c', long = "course")]
     course: Option<String>,
+
+    #[arg(
+        help = "Course title",
+        short = 'l',
+        long = "long",
+        default_value_t = false
+    )]
+    long: bool,
 }
 
-fn get_all(connection: &mut diesel::PgConnection) {
+fn get_all(connection: &mut diesel::PgConnection, args: &Args) {
     let enrolled_student_ids = enrollments::table
         .select(enrollments::student_id)
         .distinct()
@@ -30,22 +39,66 @@ fn get_all(connection: &mut diesel::PgConnection) {
     if enrolled_student_ids.is_empty() {
         println!("No enrollments found");
     } else {
-        for enrollment in enrolled_student_ids {
-            let student = users::table
-                .find(enrollment)
-                .select(User::as_select())
-                .get_result::<User>(connection)
-                .expect("Error loading student");
+        match args.long {
+            true => {
+                for enrollment in enrolled_student_ids {
+                    let user_courses = users::table
+                        .find(enrollment)
+                        .inner_join(enrollments::table.inner_join(courses::table))
+                        .select((Student::as_select(), Course::as_select()))
+                        .load::<(Student, Course)>(connection)
+                        .expect("Error loading student and enrollments");
 
-            let student_enrollments = Enrollment::belonging_to(&student)
-                .inner_join(courses::table)
-                .select(Course::as_select())
-                .load::<Course>(connection)
-                .expect("Error loading enrollments for student");
+                    for (student, course) in user_courses {
+                        println!("{} is enrolled in {}", student.name, course.title);
+                    }
+                    println!("---");
+                }
+            }
+            false => {
+                let users = users::table
+                    .select(User::as_select())
+                    .filter(users::id.eq_any(&enrolled_student_ids))
+                    .load::<User>(connection)
+                    .expect("Error loading users");
 
+                let enrollment_course_pairs = Enrollment::belonging_to(&users)
+                    .inner_join(courses::table)
+                    .load::<(Enrollment, Course)>(connection)
+                    .expect("Error loading enrollments and courses");
+
+                let student_data = enrollment_course_pairs
+                    .grouped_by(&users)
+                    .into_iter()
+                    .zip(users)
+                    .map(|(enrollments_with_courses, user)| {
+                        let courses = enrollments_with_courses
+                            .into_iter()
+                            .map(|(_enrollment, course)| course)
+                            .collect::<Vec<Course>>();
+                        StudentData {
+                            student: user,
+                            courses,
+                        }
+                    })
+                    .collect::<Vec<StudentData>>();
+                for student_data in student_data {
+                    println!(
+                        "{} is enrolled in: {}",
+                        student_data.student.name,
+                        student_data
+                            .courses
+                            .iter()
+                            .map(|course| course.title.as_str())
+                            .join(", ")
+                    )
+                }
+            }
         }
     }
 }
 fn main() {
     let args = Args::parse();
+    let mut connection = orm_module::establish_connection();
+    get_all(&mut connection, &args);
 }
