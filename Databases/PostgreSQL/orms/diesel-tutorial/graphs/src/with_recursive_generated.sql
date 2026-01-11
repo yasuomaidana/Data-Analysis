da52940d7ec336bfa5c9d840bbf91581d562ed40
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2026-01-11 11:05:40 -0600

    COUR-1961: feature: add recursive query logging and SQL definitions for account relationships

--- Databases/PostgreSQL/orms/diesel-tutorial/graphs/src/with_recursive_generated.sql
+++ Databases/PostgreSQL/orms/diesel-tutorial/graphs/src/with_recursive_generated.sql
@@ -1,5 +1,4 @@
-WITH RECURSIVE "account_relationships"
-    ("id", "_type", "metadata", "relationship_class", "target_entity_id")
+WITH RECURSIVE "account_relationships" ("id", "_type", "metadata", "relationship_class", "target_entity_id")
                    AS (SELECT "entities"."id",
                               "entities"."_type",
                               "entities"."metadata",
@@ -16,12 +15,12 @@ WITH RECURSIVE "account_relationships"
                        FROM (("entities" INNER JOIN "relationships"
                               ON ("entities"."id" = "relationships"."source_entity_id")) INNER JOIN "account_relationships"
                              ON ("account_relationships"."target_entity_id" = "entities"."id")))
-SELECT "account_relationships"."id",
-       "account_relationships"."_type",
-       "account_relationships"."metadata",
-       "account_relationships"."relationship_class",
-       "entities"."id",
-       "entities"."_type",
-       "entities"."_class",
-       "entities"."metadata"
+SELECT DISTINCT "account_relationships"."id",
+                "account_relationships"."_type",
+                "account_relationships"."metadata",
+                "account_relationships"."relationship_class",
+                "entities"."id",
+                "entities"."_type",
+                "entities"."_class",
+                "entities"."metadata"
 FROM ("account_relationships" INNER JOIN "entities" ON ("account_relationships"."target_entity_id" = "entities"."id"))
