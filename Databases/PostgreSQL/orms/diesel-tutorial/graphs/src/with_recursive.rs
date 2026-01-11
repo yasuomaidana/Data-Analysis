fc94c2d4f8a221939e3ff84caf277a08b3ad5b47
Yasuo Ignacio Maidana Perez <yasuomaidana@gmail.com>
2026-01-11 12:46:36 -0600

    COUR-1961: feature: refactor recursive SQL query implementation to use common module

--- Databases/PostgreSQL/orms/diesel-tutorial/graphs/src/with_recursive.rs
+++ Databases/PostgreSQL/orms/diesel-tutorial/graphs/src/with_recursive.rs
@@ -21,17 +21,7 @@ fn main() {
         ));
 
     // 2. Define the Recursive Term
-    let recursive = entities::table
-        .inner_join(relationships::table.on(entities::id.eq(relationships::source_entity_id)))
-        .inner_join(
-            account_relationships::table
-                .on(account_relationships::target_entity_id.eq(entities::id)),
-        )
-        .select((
-            EntityReturn::as_select(),
-            relationships::_class,
-            relationships::target_entity_id,
-        ));
+    let recursive = get_recursive();
 
     let cols = Columns::for_table::<account_relationships::table>();
 
