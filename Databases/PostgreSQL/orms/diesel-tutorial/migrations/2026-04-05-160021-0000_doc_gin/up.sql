f72862a3c4789faa693d533cc4fe92e4e8f316db
yasuomaidana <yasuomaidana@gmail.com>
2026-04-05 11:04:55 -0600

    COUR-1961 feat(database): add GIN indexing for text documents

---
+++ Databases/PostgreSQL/orms/diesel-tutorial/migrations/2026-04-05-160021-0000_doc_gin/up.sql
@@ -0,0 +1,34 @@
+-- Your SQL goes here
+CREATE TABLE gin_array_docs
+(
+    id  SERIAL,
+    doc TEXT,
+    PRIMARY KEY (id)
+);
+CREATE INDEX gin_array ON gin_array_docs USING gin (string_to_array(doc, ' ') array_ops);
+
+INSERT INTO gin_array_docs (doc)
+VALUES ('This is SQL and Python and other fun teaching stuff'),
+       ('More people should learn SQL from UMSI'),
+       ('UMSI also teaches Python and also SQL');
+
+
+INSERT INTO gin_array_docs (doc)
+SELECT 'Neon ' || generate_series(10000, 20000);
+
+CREATE TABLE gin_ts_docs
+(
+    id  SERIAL,
+    doc TEXT,
+    PRIMARY KEY (id)
+);
+CREATE INDEX gin_ts ON gin_ts_docs USING gin (to_tsvector('english', doc));
+
+INSERT INTO gin_ts_docs (doc)
+VALUES ('This is SQL and Python and other fun teaching stuff'),
+       ('More people should learn SQL from UMSI'),
+       ('UMSI also teaches Python and also SQL');
+
+-- Filler rows
+INSERT INTO gin_ts_docs (doc)
+SELECT 'Neon ' || generate_series(10000, 20000);
